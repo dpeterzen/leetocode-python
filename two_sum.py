8a38978d61c155316072ba6a448a6d2cdfdc50c6
Dan Peterson <dmpeterson317@gmail.com>
2024-07-04 14:11:14 -0400

    add brute force approach

--- two_sum.py
+++ two_sum.py
@@ -1,25 +1,9 @@
 from typing import List
 
-
 class Solution:
+    # One-pass Hash Table
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         numMap = {}
-
-        # # nums = [2,7,11,15]
-
-        # # construct the map
-        # n = len(nums)
-        # for i in range(n):
-        #     numMap[nums[i]] = i
-
-        # # create compliment and check if it exists in numMap
-        # for i in range(n):
-        #     compliment = target - nums[i]
-        #     if compliment in numMap:
-        #         return [numMap[compliment], i]
-
-        # return []
-
         n = len(nums)
 
         for i in range(n):
@@ -29,3 +13,10 @@ class Solution:
             numMap[nums[i]] = i
 
         return []  # No solution found
+    
+    # Brute Force
+    def twoSumBruteForce(self, nums: List[int], target: int) -> List[int]:
+        for i in range(len(nums)):
+            for j in range(i + 1, len(nums)):
+                if nums[j] == target - nums[i]:
+                    return [i, j]
